e9791cc95ba0d3d9c4c999da960a3210ca060f96
PhilaHillie <philaillustrates@gmail.com>
2021-10-01 23:45:04 +0200

    nav bar styling and additon of nav bar to other web pages

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 const menuItems = [
-  {title: "POPCORN CREW" , link: "./index.html"}, 
-  {title: "DESIGN" , link: "./ABOUT/about.html"},
-  {title: "THEORY BLOGS" , link: "./THEORY BLOGS/theory blogs.html"}, 
-  {title: "CONTACT" , link: "./CONTACT/contact.html"}, 
+  {title: "POPCORN CREW" , link: "/index.html"}, 
+  {title: "DESIGN" , link: "/ABOUT/about.html"},
+  {title: "THEORY BLOGS" , link: "/THEORY BLOGS/theory blogs.html"}, 
+  {title: "CONTACT" , link: "/CONTACT/contact.html"}, 
 ];
 
 window.addEventListener ('DOMContentLoaded', (event) => {
@@ -12,7 +12,11 @@ window.addEventListener ('DOMContentLoaded', (event) => {
 });
 
 const renderMenu =() => {
+
+    let pathPrefix = window.location.pathname === "/WSOA3029A_1388793/" ? "./" : "../";
+    
     const navBar = document.querySelector (".menuItems");
+
     menuItems.forEach (item => {
         
         const newItem = document.createElement("ul");
