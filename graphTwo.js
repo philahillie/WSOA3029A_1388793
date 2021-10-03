d5579793758f9937b8cc24f8569960a45c3fd352
PhilaHillie <philaillustrates@gmail.com>
2021-10-03 23:50:36 +0200

    re-labelling the x axis

--- graphTwo.js
+++ graphTwo.js
@@ -2,14 +2,14 @@ let margin = 50;
 let topMargin = 10;
 let graphHeight = 500;
 let vizTwoArea = d3.select(".vizTwo");
-let xScale = d3.scaleLinear().domain([0,4]).range([0,300]);
+let xScale = d3.scalePoint().domain(["UNDER 18", "18-29", "30-44", "45+"]).range([0,300]);
 let yScale = d3.scaleLinear().domain([0,10]).range([300,0]);
 
 let data = [
-    {x: 1, y: 7.4},
-    {x: 2, y: 9.3},
-    {x: 3, y: 9.3},
-    {x: 4, y: 9.2}
+    {x: "UNDER 18", y: 9.2},
+    {x: "18-29", y: 9.3},
+    {x: "30-44", y: 9.2},
+    {x: "45+", y: 9.1}
 ]
 
 vizTwoArea 
