1ec495b463712858ac1d6374b07177d5bab5e0d4
PhilaHillie <philaillustrates@gmail.com>
2021-10-03 23:55:32 +0200

    fixing labelling of the graphs

--- graphTwo.js
+++ graphTwo.js
@@ -1,11 +1,12 @@
-let margin = 50;
+let margin = 20;
 let topMargin = 10;
 let graphHeight = 500;
 let vizTwoArea = d3.select(".vizTwo");
-let xScale = d3.scalePoint().domain(["UNDER 18", "18-29", "30-44", "45+"]).range([0,300]);
+let xScale = d3.scalePoint().domain(["0","UNDER 18", "18-29", "30-44", "45+"]).range([0,300]);
 let yScale = d3.scaleLinear().domain([0,10]).range([300,0]);
 
 let data = [
+    
     {x: "UNDER 18", y: 9.2},
     {x: "18-29", y: 9.3},
     {x: "30-44", y: 9.2},
