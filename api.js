aaefb9ab5453131a9b827353158679cf5ad268b4
PhilaHillie <philaillustrates@gmail.com>
2021-11-08 19:28:54 +0200

    fixing the fetch api and graphs

--- api.js
+++ api.js
@@ -5,15 +5,62 @@ fetch("https://imdb8.p.rapidapi.com/title/get-ratings?tconst=tt0944947", {
 		"x-rapidapi-key": "99f5794064msh9e2d5b9a32574a3p11cc2fjsn711936ad23f9",
 	}
 })
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+	.then(response => response.json())
+	.then(jsonData => drawGraph(jsonData))
+	.catch(err => console.error(err));
 
+function drawGraph(dataToShow) {
+	let margin = 20;
+	let topMargin = 10;
+	let graphHeight = 500;
+	let vizArea = d3.select(".viz");
+	let xScale = d3.scalePoint().domain(["0", "UNDER 18", "18-29", "30-44", "45+"]).range([0, 300]);
+	let yScale = d3.scaleLinear().domain([0, 10]).range([300, 0]);
+	
+	console.log("inside function", dataToShow.ratingsHistograms);
+	const teenagers = dataToShow.ratingsHistograms["Males Aged under 18"];
+	const youngAdults = dataToShow.ratingsHistograms["Males Aged 18-29"];
+	const adults = dataToShow.ratingsHistograms["Males Aged 30-44"];
+	const olderAdults = dataToShow.ratingsHistograms["Males Aged 45+"];
 
+	let data = [
 
+		{ x: "UNDER 18", y: dataToShow.ratingsHistograms["Males Aged under 18"].aggregateRating},
+		{ x: "18-29", y: youngAdults.aggregateRating},
+		{ x: "30-44", y: adults.aggregateRating},
+		{ x: "45+", y: olderAdults.aggregateRating}
+	]
+
+	vizArea
+		.append("g")
+		.attr("transform", `translate (${margin}, ${topMargin})`)
+		.call(d3.axisLeft(yScale));
+
+	vizArea
+		.append("g")
+		.attr("transform", `translate (${margin}, 310)`)
+		.call(d3.axisBottom(xScale));
+
+	//let x = d3.ScalePoint()
+	//.domain(["UNDER 18", "18-29", "30-44", "45+"])
+	//.range([300,0])
+
+	vizArea
+		.selectAll("dots")
+		.data(data)
+		.enter()
+		.append("circle", "fill", 255)
+		.attr("cx", function (d) {
+			return xScale(d.x);
+		})
+		.attr("cy", function (d) {
+			return yScale(d.y);
+		})
+		.attr("r", 5);
+
+}
+
+//showData(4);
 
 //fetch("https://imdb8.p.rapidapi.com/title/get-most-popular-movies?homeCountry=US&purchaseCountry=US&currentCountry=US", {
 //	"method": "GET",
--- graphOne.js
+++ graphOne.js
@@ -1,44 +1,44 @@
-let margin = 20;
-let topMargin = 10;
-let graphHeight = 500;
-let vizArea = d3.select(".viz");
-let xScale = d3.scalePoint().domain(["0","UNDER 18", "18-29", "30-44", "45+"]).range([0,300]);
-let yScale = d3.scaleLinear().domain([0,10]).range([300,0]);
+// let margin = 20;
+// let topMargin = 10;
+// let graphHeight = 500;
+// let vizArea = d3.select(".viz");
+// let xScale = d3.scalePoint().domain(["0","UNDER 18", "18-29", "30-44", "45+"]).range([0,300]);
+// let yScale = d3.scaleLinear().domain([0,10]).range([300,0]);
 
-let data = [
+// let data = [
     
-    {x: "UNDER 18", y: 9.2},
-    {x: "18-29", y: 9.3},
-    {x: "30-44", y: 9.2},
-    {x: "45+", y: 9.1}
-]
+//     {x: "UNDER 18", y: 9.2},
+//     {x: "18-29", y: 9.3},
+//     {x: "30-44", y: 9.2},
+//     {x: "45+", y: 9.1}
+// ]
 
-vizArea 
- .append("g")
- .attr("transform", `translate (${margin}, ${topMargin})`)
- .call (d3.axisLeft(yScale));
+// vizArea 
+//  .append("g")
+//  .attr("transform", `translate (${margin}, ${topMargin})`)
+//  .call (d3.axisLeft(yScale));
 
- vizArea
- .append("g")
- .attr("transform", `translate (${margin}, 310)`)
- .call (d3.axisBottom(xScale));
+//  vizArea
+//  .append("g")
+//  .attr("transform", `translate (${margin}, 310)`)
+//  .call (d3.axisBottom(xScale));
 
-//let x = d3.ScalePoint()
-//.domain(["UNDER 18", "18-29", "30-44", "45+"])
-//.range([300,0])
+// //let x = d3.ScalePoint()
+// //.domain(["UNDER 18", "18-29", "30-44", "45+"])
+// //.range([300,0])
 
-vizArea 
-.selectAll("dots")
-.data(data)
-.enter()
-.append("circle", "fill", 255)
-.attr("cx",function(d){
-      return xScale(d.x);
-})
-.attr("cy", function(d){
-    return yScale (d.y);
-})
-.attr("r", 5)
+// vizArea 
+// .selectAll("dots")
+// .data(data)
+// .enter()
+// .append("circle", "fill", 255)
+// .attr("cx",function(d){
+//       return xScale(d.x);
+// })
+// .attr("cy", function(d){
+//     return yScale (d.y);
+// })
+// .attr("r", 5)
 
 
 
