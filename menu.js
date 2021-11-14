86d09ebb02f3f6a04987d0c8bed23cdaa2952f80
PhilaHillie <philaillustrates@gmail.com>
2021-11-14 14:56:11 +0200

    fixing nav bar links

--- menu.js
+++ menu.js
@@ -1,26 +1,26 @@
 const menuItems = [
-  {title: "POPCORN CREW" , link: "./index.html"}, 
-  {title: "DESIGN" , link: "./ABOUT/about.html"},
-  {title: "THEORY BLOGS" , link: "./THEORY BLOGS/theory blogs.html"}, 
-  {title: "CONTACT" , link: "./CONTACT/contact.html"}, 
+  {title: "POPCORN CREW", link: "/index.html"}, 
+  {title: "DESIGN", link: "/ABOUT/about.html"},
+  {title: "THEORY BLOGS", link: "/THEORY BLOGS/theory blogs.html"}, 
+  {title: "CONTACT", link: "/CONTACT/contact.html"}, 
 ];
 
 window.addEventListener ('DOMContentLoaded', (event) => {
-    console.log ('DOM fully loaded and parsed');
+    console.log('DOM fully loaded and parsed');
     renderMenu();
 
 });
 
-const renderMenu =() => {
+const renderMenu = ()=> {
 
-    let pathPrefix = window.location.pathname === "/WSOA3029A_1388793/" ? "./" : "../";
+    let pathPrefix = window.location.pathname === "/WSOA3029A_1388793/" ? "./" : "../"
     
-    const navBar = document.querySelector (".menuItems");
+    const navBar = document.querySelector(".menuItems");
 
-    menuItems.forEach (item => {
+    menuItems.forEach(item => {
         
         const newItem = document.createElement("ul");
-        const newLink = document.createElement ("a");
+        const newLink = document.createElement("a");
         newLink.href = item.link;
         newLink.text = item.title;
         newItem.appendChild(newLink);
