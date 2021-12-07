d064524c4f38a2ba0f06434c6615a45762e69738
PhilaHillie <philaillustrates@gmail.com>
2021-12-07 16:46:58 +0200

    uncancelling fetch command so api can load

--- graphTwo.js
+++ graphTwo.js
@@ -1,13 +1,13 @@
-fetch("https://imdb8.p.rapidapi.com/title/get-ratings?tconst=tt0944947", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "imdb8.p.rapidapi.com",
-		"x-rapidapi-key": "99f5794064msh9e2d5b9a32574a3p11cc2fjsn711936ad23f9",
-	}
-})
-	.then(response => response.json())
-	.then(jsonData => drawGraph(jsonData))
-	.catch(err => console.error(err));
+ fetch("https://imdb8.p.rapidapi.com/title/get-ratings?tconst=tt0944947", {
+ 	"method": "GET",
+ 	"headers": {
+ 		"x-rapidapi-host": "imdb8.p.rapidapi.com",
+ 		"x-rapidapi-key": "99f5794064msh9e2d5b9a32574a3p11cc2fjsn711936ad23f9",
+ 	}
+ })
+ 	.then(response => response.json())
+ 	.then(jsonData => drawGraph(jsonData))
+ 	.catch(err => console.error(err));
 
 function drawGraph(dataToShow) {
 	let margin = 30;
